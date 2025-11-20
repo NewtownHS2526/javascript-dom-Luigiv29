309058b568194e9032814894a4e90ee3d4528a5a
Luigi Villalba <luigiv3@nycstudents.net>
2025-11-20 12:08:11 -0500

    update

--- exercises/07-comparison-operators/level-01/exercise-3/script.js
+++ exercises/07-comparison-operators/level-01/exercise-3/script.js
@@ -1,3 +1,6 @@
 // TODO: Compare numbers using >
 // Example: console.log(20 > 18); // true
 
+let num = 20;
+console.log(num > 18);
+console.log(num > 25);
--- exercises/07-comparison-operators/level-02/exercise-3/script.js
+++ exercises/07-comparison-operators/level-02/exercise-3/script.js
@@ -1,3 +1,6 @@
 // TODO: Use <= operator
 // Example: console.log(20 <= 25); // true
 
+let temp = 20;
+console.log(temp <= 25);
+console.log(temp <= 15);
