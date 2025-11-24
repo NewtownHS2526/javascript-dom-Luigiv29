47cdd99d51165b350a7b2c6266c14c851dd523a5
Luigi Villalba <luigiv3@nycstudents.net>
2025-11-24 12:02:45 -0500

    seven-complete

--- exercises/07-comparison-operators/level-08/exercise-1/example.js
+++ exercises/07-comparison-operators/level-08/exercise-1/example.js
@@ -3,7 +3,7 @@
 
 let a = 5;
 let b = 5;
-console.log(a === b); // true
+console.log(a === b);  // true
 console.log(5 === "5"); // false
 
 // Try variations:
--- exercises/07-comparison-operators/level-11/exercise-1/script.js
+++ exercises/07-comparison-operators/level-11/exercise-1/script.js
@@ -1,3 +1,7 @@
 // TODO: Compare two values using ===
 // Example: console.log(5 === 5); // true
 
+let a = 5;
+let b = 10;
+console.log(a !== b);
+console.log(5 !== 10);
